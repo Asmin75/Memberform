3c044071a29059dd5b55a7fe252c814b09a4f7af
Asmin75 <asmin7@gmail.com>
2019-03-13 10:21:16 +0545

    added search options and filters in admin

--- memform/admin.py
+++ memform/admin.py
@@ -4,8 +4,20 @@ import django.contrib.auth.models
 from django.contrib import auth
 from .models import Member
 
-
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('email_address', 'title', 'first_name', 'middle_name',
+                    'last_name', 'date_of_birth', 'gender', 'program_participated',
+                    'program_start_year', 'program_start_year', 'program_end_year',
+                    'current_designation', 'current_organization', 'business_address', 'residence_address',
+                    'personal_email', 'office_email', 'office_phone', 'mobile_phone', 'photo', 'social_media_address',
+                    )
+    search_fields = ('email_address', 'title', 'first_name', 'middle_name',
+                    'last_name', 'date_of_birth', 'gender', 'program_participated',
+                    'program_start_year', 'program_start_year', 'program_end_year',
+                    'current_designation', 'current_organization', 'business_address', 'residence_address',
+                    'personal_email', 'office_email', 'office_phone', 'mobile_phone', 'photo', 'social_media_address',
+                    )
 admin.site.unregister(auth.models.User)
 admin.site.unregister(auth.models.Group)
-admin.site.register(Member)
+admin.site.register(Member,MemberAdmin)
 # Register your models here.
